2057995d2ec9ae432d23ed11c9c59a465c101086
jdmart <james.d.martino@gmail.com>
2015-10-26 20:45:34 -0400

    Corrected bash script to run CADD input files from CADD_FreeSurf/FreeSurf directory using the CADD executable in CADD_FreeSurf

--- FreeSurf/run_head_node.sh
+++ FreeSurf/run_head_node.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 #
-echo executable path: /home/jmartino/CADD_latest/build/CADD
-echo input file path: /net/angmar/home/jmartino/CADD_latest/CADD_input/Army_CSAT/free_surface/brittle2.inp
+echo executable path: /net/angmar/home/jmartino/CADD_FreeSurf/build/CADD
+echo input file path: /net/angmar/home/jmartino/CADD_FreeSurf/FreeSurf/brittle2.inp
 #
-/home/jmartino/CADD_latest/build/CADD < /net/angmar/home/jmartino/CADD_latest/CADD_input/Army_CSAT/free_surface/brittle2.inp > output
+/net/angmar/home/jmartino/CADD_FreeSurf/build/CADD < /net/angmar/home/jmartino/CADD_FreeSurf/FreeSurf/brittle2.inp > output
